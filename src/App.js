52d1e25491036110c01d067b21e1ffb35090ae28
Raj Gupta <116059009+CoderRajg@users.noreply.github.com>
2023-04-18 16:58:11 +0530

    sidebar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Twitter clone</h1>
+      {/* Sidebar- on the left */}
+      <Sidebar/>
+
+
+      {/* Feed- only scrolable thing */}
+
+      {/* Widgets- the cards in the right hand */}
+    </>
   );
 }
 
---
+++ src/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './sidebar.css';
+import SidebarOption from './SidebarOption';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import HomeIcon from '@mui/icons-material/Home';
+import ExploreIcon from '@mui/icons-material/Explore';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import MessageIcon from '@mui/icons-material/Message';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { Button } from '@mui/material';
+
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      {/* twitter icon */}
+      <TwitterIcon/>
+
+      {/* othericons */}
+      <SidebarOption active Icon={HomeIcon} text="Home"/>
+      <SidebarOption Icon={ExploreIcon} text="Explore"/>
+      <SidebarOption Icon={NotificationsNoneIcon} text="Notification"/>
+      <SidebarOption Icon={MessageIcon} text="Messages"/>
+      <SidebarOption Icon={BookmarkIcon} text="Bookmark"/>
+      <SidebarOption Icon={AccountCircleIcon} text="Profile"/>
+      <SidebarOption Icon={MoreHorizIcon} text="More"/>
+      
+      {/* buttons of tweet */}
+      <Button variant='outlined' className='tweet-btn' fullWidth>Tweet</Button>
+    </div>
+  )
+}
+
+export default Sidebar
